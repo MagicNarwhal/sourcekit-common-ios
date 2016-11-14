bdad752bae8aa5f520c685688f27291d24e26b11
Timothy Sanders <tsanders@hiddenjester.com>
2016-11-14 12:17:40 -0800

    Added a framework version of SourceKitCommon

---
+++ src/SourceKitCommon/SourceKitCommon.h
@@ -0,0 +1,24 @@
+//
+//  SourceKitCommon.h
+//  SourceKitCommon
+//
+//  Created by Timothy Sanders on 11/14/16.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SourceKitCommon.
+FOUNDATION_EXPORT double SourceKitCommonVersionNumber;
+
+//! Project version string for SourceKitCommon.
+FOUNDATION_EXPORT const unsigned char SourceKitCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SourceKitCommon/PublicHeader.h>
+#import "SourceKitCommon/SKCommonSettings.h"
+#import "SourceKitCommon/SKBrowser.h"
+#import "SourceKitCommon/SKBrowserControlsView.h"
+#import "SourceKitCommon/SKLogger.h"
+#import "SourceKitCommon/SKReachability.h"
+
+
+
